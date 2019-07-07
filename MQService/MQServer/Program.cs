d5a5d4503b70474b1bd1dd296af16922e2672eeb
350951061 <350951061@qq.com>
2019-07-07 09:36:58 +0800

    新建了一个MQ的项目

---
+++ MQService/MQServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MQServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
